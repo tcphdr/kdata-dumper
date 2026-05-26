14e8029e077cd3e2b148725a5f051084c904916c
darkness <73371463+tcphdr@users.noreply.github.com>
2026-05-26 03:11:43 -0500

    Update main.h

--- main.h
+++ main.h
@@ -55,8 +55,7 @@ typedef struct {
 
 int sceKernelSendNotificationRequest(int, notify_request_t*, size_t, int);
 
-// Declerations
-static u32 get_fw_version(void);
-static size_t get_kdata_size(u64 kdata_base);
-static void notify(const char *fmt, ...);
+// Functions.
+void notify(const char *fmt, ...);
+int gpu_dma_setup(u64 curproc, u64 proc_vmspace_off);
 int dump_to_file_gpu(const char *path, u64 kdata_base, size_t total_size, u64 curproc, u64 proc_vmspace_off);
